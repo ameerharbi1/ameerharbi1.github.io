42f0a02f3ebbfc766d09b8622497ce6d77e163c0
ameerharbi1 <74011429+ameerharbi1@users.noreply.github.com>
2024-07-17 16:09:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -82,3 +82,24 @@ function animate() {
 }
 
 animate();
+
+const scrollers = document.querySelectorAll(".hero-2");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".logos");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
